126bfe76e6acd00ef98cd910381a32b80f33d4c5
Sorbet99 <emmanuelle.lpl@gmail.com>
2025-02-06 18:02:34 +0000

    Première modification du titre, sous-titre et bins en newbins

--- app.R
+++ app.R
@@ -1,11 +1,13 @@
 library(shiny)
 
 ui <- fluidPage(
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("My First Shimy App"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-        "Number of bins:",
+      h2("My app from scratch"),
+      sliderInput(
+        inputId = "newbins",
+        label = "Number of bins:",
         min = 1,
         max = 50,
         value = 30
@@ -20,7 +22,7 @@ ui <- fluidPage(
 server <- function(input, output) {
   output$distPlot <- renderPlot({
     x <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    bins <- seq(min(x), max(x), length.out = input$newbins + 1)
 
     hist(x,
       breaks = bins, col = "darkgray", border = "white",
